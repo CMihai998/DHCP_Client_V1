879c3a9a0e4624b184c0e3b9dd54ae117fa58aac
Mihai <mihai3198@gmail.com>
2021-04-19 00:37:39 +0300

    Implemented functions to get the PublicKey and AllowedIPS and to write the new configuration to file

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
+#define _GNU_SOURCE     /* To get defns of NI_MAXSERV and NI_MAXHOST */
+#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
+#include <ifaddrs.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <linux/if_link.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
+#define WG_NAME "wg0"
 #define DHCP_PORT 6969
+#define OLD_CONFIG_FILE "/etc/wireguard/wg0.conf"
+#define NEW_CONFIG_FILE "/etc/wireguard/wg1.conf"
+
+char PUBLIC_KEY[256], ALLOWED_IPS[256];
 
 void error(char *message) {
     perror(message);
@@ -98,7 +111,218 @@ void shutdown_server(int sock, struct sockaddr_in *server, int server_length) {
 
 }
 
+
+void check_for_shutdown(int sock, struct sockaddr_in *server, int server_length) {
+    struct ifaddrs *ifaddr;
+    bool found;
+
+
+    LOOP:
+
+        found = false;
+        if (getifaddrs(&ifaddr) == -1)
+            error("getifaddrs() - check_for_shutdown()");
+
+        for (struct ifaddrs *ifa = ifaddr; ifa != NULL && found == false;
+             ifa = ifa->ifa_next) {
+
+            if (ifa->ifa_addr == NULL)
+                continue;
+
+            if (strcmp(ifa->ifa_name, WG_NAME) == 0)
+                found = true;
+        }
+
+        freeifaddrs(ifaddr);
+
+        if (found == false) {
+            return_address(sock, server, server_length, NULL);
+            exit(EXIT_SUCCESS);
+        }
+
+        sleep(120);
+
+    goto LOOP;
+
+}
+
+
+void usage() {
+    int sock, server_length, n;
+    struct sockaddr_in *server = (struct sockaddr_in*) malloc(sizeof (struct sockaddr_in));
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0)
+        error("socket()");
+
+    server->sin_family = AF_INET;
+    server->sin_addr.s_addr = INADDR_ANY;
+    server->sin_port = htons(DHCP_PORT);
+    server_length = sizeof (struct sockaddr_in);
+
+    struct in_addr* address = receive_address(sock, server, server_length);
+    //setup wg0 with address
+    //send allowed ips to server
+    //send public key to server
+    //spawn_check(sock, server, server_length_address)
+    //check died, exit || return address and exit
+}
+
+void get_data_from_config_file() {
+    bool public_key_found = false, allowed_ips_found = false;
+    char line[512], *word_list[64], delimit[]=" ";
+    FILE *fp;
+    int i;
+    fp = fopen(OLD_CONFIG_FILE, "r");
+
+    if (fp == NULL)
+        error("fopen() - CONFIG_FILE");
+
+    while (fgets(line, 512, fp)) {
+        i = 0;
+        word_list[i] = strtok(line, delimit);
+        while (word_list[i] != NULL)
+            word_list[++i] = strtok(NULL, delimit);
+
+        if (strcmp(word_list[0], "PublicKey") == 0) {
+            strcpy(PUBLIC_KEY, word_list[2]);
+            public_key_found = true;
+        }
+        if (strcmp(word_list[0], "AllowedIPs") == 0) {
+            strcpy(ALLOWED_IPS, word_list[2]);
+            allowed_ips_found = true;
+        }
+
+        if (public_key_found && allowed_ips_found) {
+            fclose(fp);
+            printf("\tPUBLIC_KEY = %s", PUBLIC_KEY);
+            printf("\n\tALLOWED_IPS = %s", ALLOWED_IPS);
+            return;
+        }
+    }
+
+    close(fp);
+    error("get_data_from_config_file() "
+          "- INVALID CONFIG FILE! PublicKey AND AllowedIPs are mandatory");
+}
+
+void write_address_to_file(struct in_addr *address) {
+    char new_content[262144], line[512], *word_list[64], delimit[]=" ";
+    char *readable_address = inet_ntoa(*address);
+    FILE *input_file, *new_file;
+    bool address_written = false;
+    int i;
+
+    input_file = fopen(OLD_CONFIG_FILE, "r");
+    if (input_file == NULL)
+        error("fopen() - OLD_CONFIG_FILE");
+
+    while (fgets(line, 512, input_file)) {
+        i = 0;
+
+        if (!address_written) {
+            word_list[i] = strtok(line, delimit);
+            while (word_list[i] != NULL) {
+                if (i == 2 && strcmp("Address", word_list[0]) == 0) {
+                    int length = strlen(new_content);
+                    new_content[length - 1] = ' ';
+                    new_content[length] = '=';
+                    new_content[length + 1] = ' ';
+                    new_content[length + 2] = '\0';
+                    strcat(new_content, readable_address);
+                    strcat(new_content, "\n");
+                    address_written = true;
+                } else {
+                    strcat(new_content, word_list[i]);
+                }
+                word_list[++i] = strtok(NULL, delimit);
+            }
+        } else {
+            strcat(new_content, line);
+        }
+    }
+    close(input_file);
+    printf("%s", new_content);
+
+    new_file = fopen(NEW_CONFIG_FILE, "w");
+    if (new_file == NULL)
+        error("fopen() - NEW_CONFIG_FILE");
+
+    fputs(new_content, new_file);
+    close(new_file);
+}
+
 int main() {
+//    char *address;
+//    struct in_addr *addr = (struct in_addr*) malloc(sizeof (struct in_addr));
+//    addr->s_addr = 1;
+//    write_address_to_file(addr);
+    get_data_from_config_file();
+    return 0;
+}
+
+
+
+void mmight_be_useful() {
+    struct ifaddrs *ifaddr;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Walk through linked list, maintaining head pointer so we
+       can free list later. */
+
+    for (struct ifaddrs *ifa = ifaddr; ifa != NULL;
+         ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+
+        /* Display interface name and family (including symbolic
+           form of the latter for the common families). */
+
+        printf("%-8s %s (%d)\n",
+               ifa->ifa_name,
+               (family == AF_PACKET) ? "AF_PACKET" :
+               (family == AF_INET) ? "AF_INET" :
+               (family == AF_INET6) ? "AF_INET6" : "???",
+               family);
+
+        /* For an AF_INET* interface address, display the address. */
+
+        if (family == AF_INET || family == AF_INET6) {
+            s = getnameinfo(ifa->ifa_addr,
+                            (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                            sizeof(struct sockaddr_in6),
+                            host, NI_MAXHOST,
+                            NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+
+            printf("\t\taddress: <%s>\n", host);
+
+        } else if (family == AF_PACKET && ifa->ifa_data != NULL) {
+            struct rtnl_link_stats *stats = ifa->ifa_data;
+
+            printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
+                   "\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
+                   stats->tx_packets, stats->rx_packets,
+                   stats->tx_bytes, stats->rx_bytes);
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    exit(EXIT_SUCCESS);
+}
+
+void might_be_useful_v2() {
     int sock, server_length, n;
     struct sockaddr_in *server = (struct sockaddr_in*) malloc(sizeof (struct sockaddr_in));
 
@@ -119,7 +343,4 @@ int main() {
     return_address(sock, server, server_length, a2);
 
     shutdown_server(sock, server, server_length);
-    return 0;
 }
-
-
